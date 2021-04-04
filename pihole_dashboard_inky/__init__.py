b0bc278a999f5bdc530696bd50fca31fbba8cfd3
numbermonkey <numbermonkey@gmail.com>
2021-04-04 20:56:21 +0100

    Update __init__.py

--- pihole_dashboard_inky/__init__.py
+++ pihole_dashboard_inky/__init__.py
@@ -118,12 +118,12 @@ def update():
 	load5min = output[-2]
 	#Just trying something#
 	last_idle = last_total = 0
-		with open('/proc/stat') as f:
-			fields = [float(column) for column in f.readline().strip().split()[1:]]
-			idle, total = fields[3], sum(fields)
-		idle_delta, total_delta = idle - last_idle, total - last_total
-		last_idle, last_total = idle, total
-		utilisation = 100.0 * (1.0 - idle_delta / total_delta)
+	with open('/proc/stat') as f:
+		fields = [float(column) for column in f.readline().strip().split()[1:]]
+		idle, total = fields[3], sum(fields)
+	idle_delta, total_delta = idle - last_idle, total - last_total
+	last_idle, last_total = idle, total
+	utilisation = 100.0 * (1.0 - idle_delta / total_delta)
 
 # Get Pihole Status
 	cmd = "/usr/local/bin/pihole status"
