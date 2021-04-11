904047b3e417f421e27afd8b7cd5cc6fd9144633
numbermonkey <numbermonkey@gmail.com>
2021-04-12 00:44:28 +0100

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -1,47 +1,66 @@
-from __future__ import print_function
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+from apiclient.discovery import build
+from apiclient import errors
+from httplib2 import Http
+from oauth2client import file, client, tools
+from email.mime.text import MIMEText
+from base64 import urlsafe_b64encode
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
-def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+SENDER = <sender>
+RECIPIENT = <recipient>
+SUBJECT = <subject>
+CONTENT = <content>
+SCOPE = 'https://www.googleapis.com/auth/gmail.compose' # Allows sending only, not reading
 
-    service = build('gmail', 'v1', credentials=creds)
+# Initialize the object for the Gmail API
+# https://developers.google.com/gmail/api/quickstart/python
+store = file.Storage('credentials.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_secret.json', SCOPE)
+    creds = tools.run_flow(flow, store)
+service = build('gmail', 'v1', http=creds.authorize(Http()))
 
-    # Call the Gmail API
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
 
-    if not labels:
-        print('No labels found.')
-    else:
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
+# https://developers.google.com/gmail/api/guides/sending
+def create_message(sender, to, subject, message_text):
+  """Create a message for an email.
+  Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+  Returns:
+    An object containing a base64url encoded email object.
+  """
+  message = MIMEText("This is the text")
+  message['to'] = numbermonkey+PiAlerts@gmail.com
+  message['from'] = pi@alert.me
+  message['subject'] = "* * Pi Alert * *"
+  encoded_message = urlsafe_b64encode(message.as_bytes())
+  return {'raw': encoded_message.decode()}
 
-if __name__ == '__main__':
-    main()
+
+# https://developers.google.com/gmail/api/guides/sending
+def send_message(service, user_id, message):
+  """Send an email message.
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+  Returns:
+    Sent Message.
+  """
+  try:
+    message = (service.users().messages().send(userId=user_id, body=message)
+               .execute())
+    print('Message Id: %s' % message['id'])
+    return message
+  #except errors.HttpError, error:
+  except:
+    print('An error occurred: %s' % error)
+
+
+raw_msg = create_message(SENDER, RECIPIENT, SUBJECT, CONTENT)
+send_message(service, "me", raw_msg)
