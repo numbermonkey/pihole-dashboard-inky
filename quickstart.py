9b14b31b5947a90e3c46a0d95087b1befd88962f
numbermonkey <numbermonkey@gmail.com>
2021-04-13 01:48:26 +0100

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -20,75 +20,73 @@ SCOPES = ['https://mail.google.com/']
 our_email = 'numbermonkey@gmail.com'
 
 def gmail_authenticate():
-    creds = None
-    # the file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first time
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
+	creds = None
+	# the file token.pickle stores the user's access and refresh tokens, and is
+	# created automatically when the authorization flow completes for the first time
+	if os.path.exists("token.pickle"):
+		with open("token.pickle", "rb") as token:
 			creds = pickle.load(token)
-    # if there are no (valid) credentials availablle, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('/home/dietpi/client_secret.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # save the credentials for the next run
-        with open("token.pickle", "wb") as token:
-            pickle.dump(creds, token)
-    return build('gmail', 'v1', credentials=creds)
+	# if there are no (valid) credentials availablle, let the user log in.
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file('/home/dietpi/client_secret.json', SCOPES)
+			creds = flow.run_local_server(port=0)
+		# save the credentials for the next run
+		with open("token.pickle", "wb") as token:
+			pickle.dump(creds, token)
+	return build('gmail', 'v1', credentials=creds)
 
 # get the Gmail API service
 service = gmail_authenticate()
 
 # Adds the attachment with the given filename to the given message
 def add_attachment(message, filename):
-    content_type, encoding = guess_mime_type(filename)
-    if content_type is None or encoding is not None:
-        content_type = 'application/octet-stream'
-    main_type, sub_type = content_type.split('/', 1)
-    if main_type == 'text':
-        fp = open(filename, 'rb')
-        msg = MIMEText(fp.read().decode(), _subtype=sub_type)
-        fp.close()
-    elif main_type == 'image':
-        fp = open(filename, 'rb')
-        msg = MIMEImage(fp.read(), _subtype=sub_type)
-        fp.close()
-    elif main_type == 'audio':
-        fp = open(filename, 'rb')
-        msg = MIMEAudio(fp.read(), _subtype=sub_type)
-        fp.close()
-    else:
-        fp = open(filename, 'rb')
-        msg = MIMEBase(main_type, sub_type)
-        msg.set_payload(fp.read())
-        fp.close()
-    filename = os.path.basename(filename)
-    msg.add_header('Content-Disposition', 'attachment', filename=filename)
-    message.attach(msg)
+	content_type, encoding = guess_mime_type(filename)
+	if content_type is None or encoding is not None:
+		content_type = 'application/octet-stream'
+	main_type, sub_type = content_type.split('/', 1)
+	if main_type == 'text':
+		fp = open(filename, 'rb')
+		msg = MIMEText(fp.read().decode(), _subtype=sub_type)
+		fp.close()
+	elif main_type == 'image':
+		fp = open(filename, 'rb')
+		msg = MIMEImage(fp.read(), _subtype=sub_type)
+		fp.close()
+	elif main_type == 'audio':
+		fp = open(filename, 'rb')
+		msg = MIMEAudio(fp.read(), _subtype=sub_type)
+		fp.close()
+	else:
+		fp = open(filename, 'rb')
+		msg = MIMEBase(main_type, sub_type)
+		msg.set_payload(fp.read())
+		fp.close()
+	filename = os.path.basename(filename)
+	msg.add_header('Content-Disposition', 'attachment', filename=filename)
+	message.attach(msg)
 	
 def build_message(destination, obj, body, attachments=[]):
-    if not attachments: # no attachments given
-        message = MIMEText(body)
-        message['to'] = destination
-        message['from'] = our_email
-        message['subject'] = obj
-    else:
-        message = MIMEMultipart()
-        message['to'] = destination
-        message['from'] = our_email
-        message['subject'] = obj
-        message.attach(MIMEText(body))
-        for filename in attachments:
-            add_attachment(message, filename)
-    return {'raw': urlsafe_b64encode(message.as_bytes()).decode()}
+	if not attachments: # no attachments given
+		message = MIMEText(body)
+		message['to'] = destination
+		message['from'] = our_email
+		message['subject'] = obj
+	else:
+		message = MIMEMultipart()
+		message['to'] = destination
+		message['from'] = our_email
+		message['subject'] = obj
+		message.attach(MIMEText(body))
+		for filename in attachments:
+			add_attachment(message, filename)
+	return {'raw': urlsafe_b64encode(message.as_bytes()).decode()}
 	
 def send_message(service, destination, obj, body, attachments=[]):
-    return service.users().messages().send(
-      userId="me",
-      body=build_message(destination, obj, body, attachments)
-    ).execute()
+	return service.users().messages().send(
+		userId="me",
+		body=build_message(destination, obj, body, attachments)).execute()
 	
-send_message(service, "numbermonkey@gmail", "* * This is a phalert * *", 
-            "Emergency. There's an emergency going on", ["/home/dietpi/test.txt", "/home/dietpi/client_secret.json"])
+send_message(service, "numbermonkey@gmail", "* * This is a phalert * *", "Emergency. There's an emergency going on", ["/hom/dietpi/test.txt", "/home/dietpi/client_secret.json"])
